7e1902d337b87cb082f0a5626798eabf3f2d4bea
Rodolfo Francisco <139409376+rflddev@users.noreply.github.com>
2025-04-02 20:47:22 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+largura = float(input('Qual a largura de sua parede?'))
+altura = float(input('Qual a altura de sua parede?'))
+
+area = largura * altura
+
+print('sua parede de {}x{} tem a area de {}'.format (largura,altura,area))
